c0afdd09f676c8cf41f8c950b6e9c09210360382
JocelynLlamas <70532285+JocelynLlamas@users.noreply.github.com>
2023-08-29 17:18:16 -0600

    Navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,97 @@
+<div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: white;">
+        <div class="fluid-container d-flex flex-column" style="width: 100%;">
+            <div class="container">
+                <div class="row">
+                    <div class="col d-flex flex-row-reverse">
+                        <div class="col-auto p-2">
+                            <button type="button" class="btn" style="font-weight: bold; color: white;background-color: #EA99A3">Donate</button>
+                        </div>
+                        <div class="col-auto p-2 d-flex justify-content-center align-items-center ">
+                            Tiendita
+                        </div>
+                        <div class="col-auto p-2 d-flex justify-content-center align-items-center">
+                            Blog
+                        </div>
+                        <div class="col-auto p-2 d-flex justify-content-center align-items-center">
+                            Chapters
+                        </div>
+                        <div class="col-auto dropdown p-2">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: black;">
+                                Founders & Investors
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-auto dropdown p-2">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: black;">
+                                Get involved
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col mt-2 border-top border-secondary-subtle">
+                        <div class="container d-flex flex-row">
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <img class="mt-2" src="/assets/logo.png" alt="logo">
+                            <div class="collapse navbar-collapse d-flex flex-row-reverse" id="navbarSupportedContent">
+                                <ul class="navbar-nav mb-2 mb-lg-0 ">
+                                    <li class="nav-item">
+                                        <a class="nav-link" aria-current="page" href="#" style="color:black; font-weight: bold">Dashboard</a>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color:black; font-weight: bold">
+                                            LiT Recruit
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="#">Action</a></li>
+                                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#" style="color:black; font-weight: bold">Events</a>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color:black; font-weight: bold">
+                                            Level up
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="#">Action</a></li>
+                                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#" style="color:black; font-weight: bold">LiT Community</a>
+                                    </li>
+                                    <li class="nav-item d-flex justify-content-center align-items-center p-1">
+                                        <img src="/assets/mail.png" alt="mail">
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color:black; font-weight: bold">
+                                            <img src="/assets/profilePhoto.png" alt="profile" style="width: 50%;">
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="#">Profile</a></li>
+                                            <li><a class="dropdown-item" href="#">Log Out</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</div>
