8072fe21ab817c59b011dee62ad36e827b02f63c
JocelynLlamas <70532285+JocelynLlamas@users.noreply.github.com>
2023-08-29 21:11:56 -0600

    Footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,68 @@
+<div class="container-fluid" style="background-color: #555555;">
+    <footer class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col d-flex flex-column">
+                    <div style="font-weight: bold; color: white">Latinas in Tech</div>
+                    <div style="color: #EA99A3;">About us</div>
+                    <div style="color: #EA99A3;">Careers</div>
+                    <div style="color: #EA99A3;">Press</div>
+                    <div style="color: #EA99A3;">LiT Chapters</div>
+                </div>
+                <div class="col d-flex flex-column">
+                    <div style="font-weight: bold; color: white">Contact</div>
+                    <div style="color: #EA99A3;">Partners</div>
+                    <div style="color: #EA99A3;">Technical issues</div>
+                    <div style="color: #EA99A3;">General</div>
+                    <div style="color: #EA99A3;">FaQ</div>
+                </div>
+                <div class="col d-flex flex-column">
+                    <div style="font-weight: bold; color: white">Get involved</div>
+                    <div style="color: #EA99A3;">Register as a Latina Member</div>
+                    <div style="color: #EA99A3;">Become an Ally</div>
+                    <div style="color: #EA99A3;">Become a Partner</div>
+                    <div style="color: #EA99A3;">Post your Company’s Job Vacants</div>
+                </div>
+                <div class="col d-flex flex-column">
+                    <div style="font-weight: bold; color: white">LiT Summit</div>
+                    <div style="color: #EA99A3;">2023 LiT Summit</div>
+                    <div style="color: #EA99A3;">2022 LiT Summit Recap</div>
+                </div>
+                <div class="col">
+                    <button type="button" class="btn" style="font-weight: bold; color: white;background-color: #EA99A3">Donate</button>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div style="font-weight: bold; color: white">Latinas in Tech is a 501(c)(3) non-profit organization</div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+<div class="container-fluid py-2" style="background-color: #3A3A3A ;">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="row">
+                    <!-- PONER BORDE DERECHO -->
+                    <div class="col-auto" style="font-weight: bold; color: white">
+                        © Latinas in Tech
+                    </div>
+                    <div class="col-auto" style="font-weight: bold; color: #EA99A3">
+                        Cookie Policy
+                    </div>
+                    <div class="col-auto" style="font-weight: bold; color: #EA99A3">
+                        Terms of use
+                    </div>
+                    <div class="col-auto" style="font-weight: bold; color: #EA99A3">
+                        Privacy Policy
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                Column
+            </div>
+        </div>
+    </div>
+</div>
